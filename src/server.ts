136dbc64ad61bd71def2060b544c4d97a78bcf91
NissimBet <nissimbet123@outlook.com>
2020-01-20 14:52:52 -0600

    added app file

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,6 @@
-import express from 'express';
+import app from './app';
+
 
 const PORT = process.env.PORT || 3001;
-const app = express();
-
-app.get('/', (req, res) => {
-  res.send("Hello World")
-})
 
 app.listen(PORT, () => console.log(`Example app listening on ${PORT}`))
